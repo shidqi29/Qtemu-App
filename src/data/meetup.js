e133bb9cd183c5d1a97ebcb0ac15fb7cddaf4067
shidqi29 <shidqiamirulhaq@gmail.com>
2023-09-07 08:40:29 +0700

    feat: add data meetup

---
+++ src/data/meetup.js
@@ -0,0 +1,20 @@
+export const meetup = [
+  {
+    id: 1,
+    title: "Hacktiv8 Meetup",
+    location: "Jakarta, Indonesia",
+    date: "1 September 2023",
+    members: 10,
+    organizer: "Shidqi Amirul Haq",
+    description: "Come and meet other developers in the javascript community.",
+  },
+  {
+    id: 2,
+    title: "Awesome Meetup and event",
+    location: "Jakarta, Indonesia",
+    date: "23 August 2023",
+    members: 102,
+    organizer: "Shidqi Amirul Haq",
+    description: "Come and meet other developers in the PHP community.",
+  },
+];
